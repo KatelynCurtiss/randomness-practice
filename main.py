9e397d6a1a135d24e38329b6c44a6cbf3d1aedaa
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-10-23 10:45:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,17 @@
 # October 22 2024
 # Random numbers
 
+
+import random as rd
 colors = ['purple', 'red', 'pink', 'navy']
 
-print(rd.colors)
+# print(colors)
 
+# randint ( ) method
+start = 0
+stop = 3
+#rand_integer = rd.randint(start, stop)
+rand_integer = rd.randrange(start, stop)
+
+print(f'The random number is: {rand_integer}')
+print(f'Randomly selected color = {colors[rand_integer]}')
